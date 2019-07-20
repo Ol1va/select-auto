324aca887602563f06757591afdb007eba04c5ef
ol1va <ouac.beleckaya@gmail.com>
2019-07-20 17:10:37 +0700

    Updates

---
+++ precache-manifest.e1b6f05e503314ccd1006b9400e5f1dd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad71b5b535ccf8b8616ad74ce9de1e3",
+    "url": "/select-auto/index.html"
+  },
+  {
+    "revision": "029fbfd14cad7434d6c2",
+    "url": "/select-auto/static/js/2.d9726584.chunk.js"
+  },
+  {
+    "revision": "21fdd9ccb5b45f5125a5",
+    "url": "/select-auto/static/js/main.f2b14d7e.chunk.js"
+  },
+  {
+    "revision": "9067a061ef7314180fea",
+    "url": "/select-auto/static/js/runtime~main.15168c58.js"
+  }
+]);
